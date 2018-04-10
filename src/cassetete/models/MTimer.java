20b4408fc7b080a98561ac580b4c98e2c1bb22f1
Tounaï <billon.anthony@etu.univ-lyon1.fr>
2018-04-11 01:03:08 +0200

    Mostly working

---
+++ src/cassetete/models/MTimer.java
@@ -0,0 +1,54 @@
+package cassetete.models;
+
+import javafx.application.Platform;
+
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MTimer extends Observable {
+    private int sec;
+    private Timer t;
+    public void start(){
+        t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        sec++;
+                        setChanged();
+                        notifyObservers();
+                    }
+                });
+
+            }
+        }, 1000, 1000);
+
+    }
+
+    public void stop(){
+        t.cancel();
+    }
+
+    public int getTimerValue(){
+        return sec;
+    }
+
+    public String getFormatedTime(){
+        int s = sec%60;
+        int m = sec/60;
+
+        String sstr="";
+        String mstr="";
+
+        if(s<10) sstr = "0"+s;
+        else  sstr = ""+s;
+
+        if(m<10) mstr = "0"+m;
+        else mstr = ""+m;
+
+        return mstr+":"+sstr;
+    }
+}
